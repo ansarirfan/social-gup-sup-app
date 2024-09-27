3e62c4515e39b112336656bde6653472be471bce
Irfan Ansari <103310054+ansarirfan@users.noreply.github.com>
2024-09-27 16:29:08 +0530

    Update postRequest.js

--- socialmedia/src/API/postRequest.js
+++ socialmedia/src/API/postRequest.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 
-const API = axios.create({baseURL: "http://localhost:5000/"})
+const API = axios.create({baseURL: "https://social-gup-sup-app.onrender.com/"})
 
 API.interceptors.request.use((req) => {
     if (localStorage.getItem('profile')) {
