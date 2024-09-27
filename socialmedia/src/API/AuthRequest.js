b68f37829ced23d6b6952f3fb6e0a60ca1f32494
Irfan Ansari <103310054+ansarirfan@users.noreply.github.com>
2024-09-27 16:26:23 +0530

    Update AuthRequest.js

--- socialmedia/src/API/AuthRequest.js
+++ socialmedia/src/API/AuthRequest.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
-const API = axios.create({baseURL: "http://localhost:5000/"})
+const API = axios.create({baseURL: "https://social-gup-sup-app.onrender.com"})
 export const logIn  = (formData) => API.post('/auth/login', formData )
 export const singUp  = (formData) => API.post('/auth/register', formData )
