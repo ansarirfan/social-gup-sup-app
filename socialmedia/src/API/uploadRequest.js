fe9b52e9550ae8ab0b891f6c629afa756d0f26c4
Irfan Ansari <103310054+ansarirfan@users.noreply.github.com>
2024-09-27 16:30:18 +0530

    Update uploadRequest.js

--- socialmedia/src/API/uploadRequest.js
+++ socialmedia/src/API/uploadRequest.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
-const API = axios.create({baseURL: "http://localhost:5000/"})
+const API = axios.create({baseURL: "https://social-gup-sup-app.onrender.com/"})
 export const uploadImage  = (data) => API.post('/upload/', data )
 export const uploadPost  = (data) => API.post('/post/', data )
